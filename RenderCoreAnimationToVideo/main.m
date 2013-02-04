04cddb467ae2c8f1986468c65b9d6da3405ca36e
Jan <github@geheimwerk.de>
2013-02-04 20:23:44 +0100

    Rewrite, improvements.

--- RenderCoreAnimationToVideo/main.m
+++ RenderCoreAnimationToVideo/main.m
@@ -11,6 +11,8 @@
 #import <CoreMedia/CoreMedia.h>
 #import <QuartzCore/QuartzCore.h>
 
+#define ENABLE_COMPOSITING_OVER_SOURCE_FILE	0
+
 CGPoint CGPointOffset(CGPoint p, CGFloat dx, CGFloat dy)
 {
 	return CGPointMake(p.x + dx, p.y + dy);
@@ -24,15 +26,56 @@ int main(int argc, const char * argv[])
 
 		// Based on http://stackoverflow.com/questions/6988950/avvideocompositioncoreanimationtool-on-export-only-sends-the-image-and-audio-but
 		
+#if ENABLE_COMPOSITING_OVER_SOURCE_FILE
+		NSError *error = nil;
+		NSString *inFileName = @"in.mov";
+#endif
+
+#define FRAMES_PER_SECOND	25
+
+		NSString *outFileName = @"out.mov";
+		CFTimeInterval duration = 30.0;
+		
+		CMTimeScale targetTimescale = FRAMES_PER_SECOND;
+		CMTime frameDuration = CMTimeMakeWithSeconds(1, targetTimescale);
+		
+		CGRect renderFrame = CGRectMake(0, 0, 1280, 720);
+		
+		CMTime durationTime = CMTimeMakeWithSeconds(duration, 1);
+		
 		// Composition setup.
 		AVMutableComposition *composition = [AVMutableComposition composition];
-		// ...
-		// NOTE: composition is an AVMutableComposition containing a single video
-		//		 track (30s of black in 1280 x 720).
+#if ENABLE_COMPOSITING_OVER_SOURCE_FILE
+		NSURL *sourceFileURL = [NSURL fileURLWithPath:inFileName];
+		AVURLAsset *asset = [AVURLAsset URLAssetWithURL:sourceFileURL
+												options:nil];
+		if (asset == nil) {
+			NSLog(@"Missing movie file:\n%@", sourceFileURL);
+			return EXIT_FAILURE;
+		}
+		
+		AVMutableCompositionTrack *trackA = [composition addMutableTrackWithMediaType:AVMediaTypeVideo
+																	 preferredTrackID:kCMPersistentTrackID_Invalid];
+		
+		NSArray *videoTracks = [asset tracksWithMediaType:AVMediaTypeVideo];
+		if (videoTracks.count == 0) {
+			NSLog(@"No video tracks in file:\n%@", sourceFileURL);
+			return EXIT_FAILURE;
+		}
+		
+		AVAssetTrack *sourceVideoTrack = videoTracks[0];
+		if ([trackA insertTimeRange:CMTimeRangeMake(kCMTimeZero, [asset duration])
+						ofTrack:sourceVideoTrack
+						 atTime:kCMTimeZero
+							  error:&error] == NO) {
+			NSLog(@"%@", error);
+			return EXIT_FAILURE;
+		}
+#endif
 		
 		// Create the animated layer
 		CALayer *renderAnimLayer = [CALayer layer];
-		renderAnimLayer.frame = CGRectMake(0, 0, 1280, 720);
+		renderAnimLayer.frame = renderFrame;
 		
 		renderAnimLayer.backgroundColor = CGColorCreateGenericRGB(0.3, 0.0, 0.0, 0.5);
 		
@@ -42,14 +85,14 @@ int main(int argc, const char * argv[])
 		
 		[CATransaction begin];
 		[CATransaction setDisableActions:YES];
-		[CATransaction setAnimationDuration:30.0];
+		[CATransaction setAnimationDuration:duration];
 		
 		CABasicAnimation *animation = [CABasicAnimation animation];
 		animation.fromValue = [NSValue valueWithPoint:square.position];
 		animation.toValue = [NSValue valueWithPoint:CGPointOffset(square.position, 800, 400)];
 		animation.removedOnCompletion = NO;
 		animation.beginTime = AVCoreAnimationBeginTimeAtZero;
-		animation.duration = 30.0;
+		animation.duration = duration;
 		
 		[CATransaction commit];
 		
@@ -57,30 +100,41 @@ int main(int argc, const char * argv[])
 		[renderAnimLayer addSublayer:square];
 		
 		// Create a composition
+		CMPersistentTrackID trackID = [composition unusedTrackID];
 		AVMutableVideoCompositionLayerInstruction *layerInstr1 =
+#if ENABLE_COMPOSITING_OVER_SOURCE_FILE
+		[AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:trackA];
+#else
 		[AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstruction];
-		layerInstr1.trackID = 2;
+#endif
+		layerInstr1.trackID = trackID;
 		
 		AVMutableVideoCompositionInstruction *instr = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
-		instr.timeRange = CMTimeRangeMake(kCMTimeZero, composition.duration);
-		instr.layerInstructions = [NSArray arrayWithObject:layerInstr1];
+		instr.timeRange = CMTimeRangeMake(kCMTimeZero, durationTime);
+		instr.layerInstructions = @[layerInstr1];
 		
+		CMPersistentTrackID renderTrackID = [composition unusedTrackID];
 		AVMutableVideoComposition *renderComp = [AVMutableVideoComposition videoComposition];
-		renderComp.renderSize	 = renderAnimLayer.frame.size;
-		renderComp.frameDuration = CMTimeMake(1, 30); // Normally 1,30
-		renderComp.instructions	 = [NSArray arrayWithObject:instr];
-		renderComp.animationTool = [AVVideoCompositionCoreAnimationTool videoCompositionCoreAnimationToolWithAdditionalLayer:renderAnimLayer asTrackID:2];
-		
+		renderComp.renderSize    = renderAnimLayer.frame.size;
+		renderComp.frameDuration = frameDuration;
+		renderComp.instructions  = @[instr];
+		renderComp.animationTool = [AVVideoCompositionCoreAnimationTool videoCompositionCoreAnimationToolWithAdditionalLayer:renderAnimLayer
+																												   asTrackID:renderTrackID];
+	
 		// Create an export session and export
-		AVAssetExportSession *exportSession = [AVAssetExportSession exportSessionWithAsset:composition presetName:@"AVAssetExportPreset1280x720"];
-		exportSession.outputURL = [NSURL URLWithString:@"file:///Users/eric/Desktop/toto.mov"];
-		exportSession.timeRange = CMTimeRangeMake(kCMTimeZero, CMTimeMake(30, 1));
+		AVAssetExportSession *exportSession = [AVAssetExportSession exportSessionWithAsset:composition
+																				presetName:AVAssetExportPresetAppleProRes422LPCM];
+		exportSession.outputURL = [NSURL URLWithString:outFileName];
+		exportSession.timeRange = CMTimeRangeMake(kCMTimeZero, durationTime);
 		exportSession.shouldOptimizeForNetworkUse = YES;
 		exportSession.videoComposition = renderComp;
+		exportSession.outputFileType = AVFileTypeQuickTimeMovie;
 		
-		// Just see how things have finished.
+		// Just see how things have turned out.
 		[exportSession exportAsynchronouslyWithCompletionHandler:^() {
-			NSLog(@"Export completed with status: %ld", exportSession.status);
+			NSLog(@"Export completed with status: %ld\nError message: %@", exportSession.status, exportSession.error);
+			int returnCode = (exportSession.status == AVAssetExportSessionStatusCompleted) ? EXIT_SUCCESS : EXIT_FAILURE;
+			exit(returnCode);
 		}];
 		
 		// TODO: remove once everything works and objects have been retained.
